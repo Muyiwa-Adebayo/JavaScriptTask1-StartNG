fe93648eb29ea33a2d2fbafb92d319658f52e41c
Obamaolu <muyiwa_david36@yahoo.com>
2020-03-21 21:44:01 +0100

    A JavaScript Program to display the Full Name, Course and a Loop to display even numbers according to the number of courses if even or odd numberfrom 1-200

---
+++ script.js
@@ -0,0 +1,10 @@
+var fullName = 'Adebayo Oluwamuyiwa David';
+var courses = ['HTML', 'CSS', 'JavaScript', 'UI/UX'];
+
+console.log(`Full Name: ${fullName}\nCourses: ${courses}`);
+
+for(var i=1; i<=200; i++){
+    if(i%2 === 0){
+        console.log(i);
+    }
+}
