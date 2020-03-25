7c04312bda6c5792c621209bac98ef28ded61143
Obamaolu <muyiwa_david36@yahoo.com>
2020-03-25 09:16:29 +0100

    Modified the code with a conditional operator then a loop that displays either even number or odd numbers in respect to the number of even or odd courses

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ var courses = ['HTML', 'CSS', 'JavaScript', 'UI/UX'];
 
 console.log(`Full Name: ${fullName}\nCourses: ${courses}`);
 
-for(var i=1; i<=200; i++){
-    if(i%2 === 0){
-        console.log(i);
+if(courses.length%2 === 0)
+    for(var i=1; i<=200; i++){
+        if(i%2 === 0)
+            console.log(i);
     }
-}
+else
+    for(var i=1; i<=200; i++){
+        if(i%3 === 0)
+            console.log(i);
+    }
